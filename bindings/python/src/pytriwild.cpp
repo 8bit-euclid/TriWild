9febb42ab059e78659667745be13ca96a3b60f83
8bit-euclid <8bit.euclid@gmail.com>
2025-08-31 14:35:35 -0400

    chore: Formatting and pybind11-stub support setup

--- bindings/python/src/pytriwild.cpp
+++ bindings/python/src/pytriwild.cpp
@@ -7,25 +7,12 @@
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(pytriwild, m) {
+PYBIND11_MODULE(pytriwild, m)
+{
     m.doc() = "Python bindings for TriWild - Robust Triangulation with Curve Constraints";
-    
-    m.def("triangulate", [](
-        const Eigen::MatrixXd& vertices_in,
-        const Eigen::MatrixXi& edges_in,
-        py::dict feature_info_dict,
-        double stop_quality = 10.0,
-        int max_iterations = 80,
-        int stage = 1,
-        double epsilon = 1e-3,
-        double feature_epsilon = 1e-3,
-        double target_edge_length = -1.0,
-        double edge_length_ratio = 0.02,
-        double flat_feature_angle = 10.0,
-        bool cut_outside = false,
-        const Eigen::MatrixXd& hole_points = Eigen::MatrixXd(),
-        bool mute_log = false
-    ) {
+
+    m.def("triangulate", [](const Eigen::MatrixXd &vertices_in, const Eigen::MatrixXi &edges_in, py::dict feature_info_dict, double stop_quality = 10.0, int max_iterations = 80, int stage = 1, double epsilon = 1e-3, double feature_epsilon = 1e-3, double target_edge_length = -1.0, double edge_length_ratio = 0.02, double flat_feature_angle = 10.0, bool cut_outside = false, const Eigen::MatrixXd &hole_points = Eigen::MatrixXd(), bool mute_log = false)
+          {
         // Convert Python dict to nlohmann::json
         nlohmann::json feature_info = nlohmann::json::parse(
             py::str(feature_info_dict).cast<std::string>()
@@ -48,25 +35,8 @@ PYBIND11_MODULE(pytriwild, m) {
         );
         
         // Return as tuple
-        return py::make_tuple(vertices_out, faces_out, nodes, face_nodes);
-    },
-    "Robust triangulation with curve constraints",
-    py::arg("vertices_in"),
-    py::arg("edges_in"), 
-    py::arg("feature_info") = py::dict(),
-    py::arg("stop_quality") = 10.0,
-    py::arg("max_iterations") = 80,
-    py::arg("stage") = 1,
-    py::arg("epsilon") = 1e-3,
-    py::arg("feature_epsilon") = 1e-3,
-    py::arg("target_edge_length") = -1.0,
-    py::arg("edge_length_ratio") = 0.02,
-    py::arg("flat_feature_angle") = 10.0,
-    py::arg("cut_outside") = false,
-    py::arg("hole_points") = Eigen::MatrixXd(),
-    py::arg("mute_log") = false
-    );
-    
+        return py::make_tuple(vertices_out, faces_out, nodes, face_nodes); }, "Robust triangulation with curve constraints", py::arg("vertices_in"), py::arg("edges_in"), py::arg("feature_info") = py::dict(), py::arg("stop_quality") = 10.0, py::arg("max_iterations") = 80, py::arg("stage") = 1, py::arg("epsilon") = 1e-3, py::arg("feature_epsilon") = 1e-3, py::arg("target_edge_length") = -1.0, py::arg("edge_length_ratio") = 0.02, py::arg("flat_feature_angle") = 10.0, py::arg("cut_outside") = false, py::arg("hole_points") = Eigen::MatrixXd(), py::arg("mute_log") = false);
+
     // Expose the global flag
     m.attr("keep_holes") = &triwild::do_triwild_keep_holes;
 }
